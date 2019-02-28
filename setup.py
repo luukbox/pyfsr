d13b23766d2144aad55ca0d827fa46c849c85869
Lukas <36888231+luukbox@users.noreply.github.com>
2019-02-28 14:30:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from pyfsr import name, version
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -10,9 +9,9 @@ with open('requirements.txt', 'r') as fh:
 setup(
     author='Lukas Sebastian Müller',
     author_email='lukassebastianmueller@gmail.com',
-    name=name,
+    name='pyfsr,
     url='https://github.com/luukbox/pyfsr',
-    version=version,
+    version='0.0.1',
     description='a Feedback Shift Register toolkit for python',
     long_description=long_description,
     long_description_content_type="text/markdown",
